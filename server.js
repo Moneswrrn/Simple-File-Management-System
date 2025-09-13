62686f0eda2f7730ac76d2993efe61537fd092f9
Moneswrrn <127534653+Moneswrrn@users.noreply.github.com>
2025-09-14 02:05:06 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const notesRouter = require("./routes/notes"); // import the notes routes
 
 app.use(cors());
 app.use(express.json());
-
-// Mount the notes routes at /api/notes
-app.use("/routes", notesRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
