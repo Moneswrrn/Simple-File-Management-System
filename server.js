eee9631aa49b57f1bea115c828f259ccd391ebcc
Moneswrrn <127534653+Moneswrrn@users.noreply.github.com>
2025-09-14 02:21:06 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+app.use('/routes', require('./routes/notes'));
 app.use(cors());
 app.use(express.json());
 app.use('/routes/notes', notesRouter);
